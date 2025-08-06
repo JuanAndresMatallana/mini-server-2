970e13340b3d43928a2ba76d56ed36eab2338bf2
andres <j.andres.matallana@gmail.com>
2025-08-06 00:56:16 -0500

    se agrega nueva funcion para eliminar tareas con gorillamux

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 type Task struct {
@@ -11,27 +14,76 @@ type Task struct {
 	Name string `json:"name"`
 }
 
+var tasks []Task
+var nextID = 1
+
+//una funcion para generar un handler
+
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "bienvenido a mi mundo")
 }
 
 func taskHandler(w http.ResponseWriter, r *http.Request) {
 
-	tasks := []Task{
-		{ID: 1, Name: "estudiar go"},
-		{ID: 2, Name: "Estudiar documentacion"},
-		{ID: 3, Name: "Hacer un proyecto"},
-	}
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(tasks)
+
+	if r.Method == http.MethodGet {
+		json.NewEncoder(w).Encode(tasks)
+		return
+
+	}
+	if r.Method == http.MethodPost {
+
+		var newTask Task
+		err := json.NewDecoder(r.Body).Decode(&newTask)
+		if err != nil {
+			http.Error(w, "Error al leer una tarea", http.StatusBadRequest)
+			return
+		}
+
+		newTask.ID = nextID
+		nextID++
+		tasks = append(tasks, newTask)
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(newTask)
+		return
+	}
+
+	http.Error(w, "Método no permitido", http.StatusMethodNotAllowed)
 
 }
 
-var tasks []Task
-var nextID = 1
+func deleteTaskHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	idStr := vars["id"]
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "ID invalido", http.StatusBadRequest)
+		return
+	}
+
+	for i, task := range tasks {
+
+		if task.ID == id {
+
+			tasks = append(tasks[:i], tasks[i+1:]...)
+			w.WriteHeader(http.StatusNoContent)
+			return
+
+		}
+
+	}
+	http.Error(w, "Tarea no encontrada", http.StatusNotFound)
+}
 
 func main() {
-	http.HandleFunc("/", helloHandler)
-	http.HandleFunc("/tasks", taskHandler)
-	http.ListenAndServe(":8080", nil)
+	tasks = append(tasks, Task{ID: 1, Name: "estudiar go"})
+	tasks = append(tasks, Task{ID: 2, Name: "Estudiar documentacion"})
+	tasks = append(tasks, Task{ID: 3, Name: "Hacer un proyecto"})
+	nextID = 4
+	r := mux.NewRouter()
+	r.HandleFunc("/", helloHandler)
+	r.HandleFunc("/tasks", taskHandler).Methods("GET", "POST")
+	r.HandleFunc("/tasks/{id}", deleteTaskHandler).Methods("DELETE")
+	http.ListenAndServe(":8080", r)
 }
